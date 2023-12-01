a928a33f43d2239a10ea62de2e4ddeb78167226f
adambaldwin <abaldwinva98@gmail.com>
2023-11-30 22:10:28 -0500

    Adding question script

--- deploy-scripts/CREATE_QUESTIONS_TABLE.sql
+++ deploy-scripts/CREATE_QUESTIONS_TABLE.sql
@@ -1,17 +1,19 @@
-CREATE TABLE questions (
-   id SERIAL PRIMARY KEY,
-   title VARCHAR(255) NOT NULL,
-   answer VARCHAR(255) NOT NULL,
-   points_possible INT NOT NULL,
-   question_type VARCHAR(50) NOT NULL,
-   options JSONB,
-   media JSONB,
-   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- Create table if not exists
+CREATE TABLE IF NOT EXISTS questions (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    answer VARCHAR(255) NOT NULL,
+    points_possible INT NOT NULL,
+    question_type VARCHAR(50) NOT NULL,
+    options JSONB,
+    media JSONB,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Index on the 'title' column for faster title lookups
-CREATE INDEX idx_title ON questions(title);
+-- Create index if not exists on the 'title' column for faster title lookups
+CREATE INDEX IF NOT EXISTS idx_title ON questions(title);
 
--- Index on the 'question_type' column for faster question type lookups
-CREATE INDEX idx_question_type ON questions(question_type);
+-- Create index if not exists on the 'question_type' column for faster question type lookups
+CREATE INDEX IF NOT EXISTS idx_question_type ON questions(question_type);
+
 
--- deploy-scripts/CREATE_USERS_TABLE.sql
+++ deploy-scripts/CREATE_USERS_TABLE.sql
@@ -1,15 +1,16 @@
-CREATE TABLE users (
-   id SERIAL PRIMARY KEY,
-   username VARCHAR(50) UNIQUE,
-   email VARCHAR(100) UNIQUE,
-   password_hash VARCHAR(100),  -- Store password hash instead of plaintext
-   google_id VARCHAR(50) UNIQUE, -- Store Google ID for Google login
-   is_admin BOOLEAN DEFAULT FALSE,  -- New boolean field for admin status
-   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- Create table if not exists
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE,
+    email VARCHAR(100) UNIQUE,
+    password_hash VARCHAR(100),  -- Store password hash instead of plaintext
+    google_id VARCHAR(50) UNIQUE, -- Store Google ID for Google login
+    is_admin BOOLEAN DEFAULT FALSE,  -- New boolean field for admin status
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Index for faster username lookups
-CREATE INDEX idx_username ON users(username);
+-- Create index if not exists for faster username lookups
+CREATE INDEX IF NOT EXISTS idx_username ON users(username);
 
--- Index for faster email lookups
-CREATE INDEX idx_email ON users(email);
+-- Create index if not exists for faster email lookups
+CREATE INDEX IF NOT EXISTS idx_email ON users(email);
